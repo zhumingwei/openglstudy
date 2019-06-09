a55a595566b505564b0fc2b0c3dafe1365a2c239
zhumingwei <zdf312192599@163.com>
2019-06-09 23:40:13 +0800

    画方形，修改glsl错误

--- project/firstproject/firstproject/firstproject/firstproject.cpp
+++ project/firstproject/firstproject/firstproject/firstproject.cpp
@@ -10,15 +10,15 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 300 core\n"
-"layout (location = 0)in vec3 aPos:\n"
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0)in vec3 aPos;\n"
 "void main()\n"
 "{\n"
-" gl_Position = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+" gl_Position = vec4(aPos.x, aPos.y, aPos.z,1.0f);\n"
 "}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"void mian()\n"
+"void main()\n"
 "{\n"
 " FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
 "}\n\0";
@@ -43,14 +43,6 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	//初始化glad
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
 
 	//设置窗口的维度(Dimension)：
 	//glViewport函数前两个参数控制窗口左下角的位置。第三个和第四个参数控制渲染窗口的宽度和高度
@@ -104,24 +96,35 @@ int main()
 
 	 
 	float vertices[] = {
-		-0.5f,-0.5f,0.0f,
-		0.5f,-0.5f,0.0f,
-		0.0f,0.5f,0.0f
+		0.5f,0.5f,0.0f,//右上
+		0.5f,-0.5f,0.0f,//右下
+		-0.5f,-0.5f,0.0f,//左下
+		-0.5,0.5f,0.0f//左上
 	};
-	unsigned int VBO, VAO;
+	unsigned int indices[] = {
+		0,1,3,
+		1,2,3,
+	};
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,GL_STATIC_DRAW);
 	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-
-		
 	//渲染循环
 	while (!glfwWindowShouldClose(window))
 	{
@@ -133,10 +136,12 @@ int main()
 		//画三角
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES,0,3);
+		//glDrawArrays(GL_TRIANGLES,0,3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//检查事件调用
-		glfwPollEvents();
 		glfwSwapBuffers(window);
+		glfwPollEvents();
+
 
 	}
 
