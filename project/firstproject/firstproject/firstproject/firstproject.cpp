a2af0525e8fd05da01e96d6f8a74d4cfe54df8cc
zhumingwei <zdf312192599@163.com>
2019-06-11 23:47:04 +0800

    着色器

--- project/firstproject/firstproject/firstproject/firstproject.cpp
+++ project/firstproject/firstproject/firstproject/firstproject.cpp
@@ -12,15 +12,19 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0)in vec3 aPos;\n"
+"layout (location = 1)in vec3 aColor;\n"
+"out vec3 outColor;\n"
 "void main()\n"
 "{\n"
-" gl_Position = vec4(aPos.x, aPos.y, aPos.z,1.0f);\n"
+" gl_Position = vec4(aPos,1.0f);\n"
+" outColor = aColor;\n"
 "}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 outColor;\n"
 "void main()\n"
 "{\n"
-" FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+" FragColor = vec4(outColor,1.0f);\n"
 "}\n\0";
 int main()
 {
@@ -96,28 +100,25 @@ int main()
 
 	 
 	float vertices[] = {
-		0.5f,0.5f,0.0f,//右上
-		0.5f,-0.5f,0.0f,//右下
-		-0.5f,-0.5f,0.0f,//左下
-		-0.5,0.5f,0.0f//左上
+		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		 -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
 	};
-	unsigned int indices[] = {
-		0,1,3,
-		1,2,3,
-	};
-	unsigned int VBO, VAO, EBO;
+	
+
+	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,GL_STATIC_DRAW);
 	
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glUseProgram(shaderProgram);
 
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -134,10 +135,9 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		//画三角
-		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES,0,3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES,0,3);
+		
 		//检查事件调用
 		glfwSwapBuffers(window);
 		glfwPollEvents();
