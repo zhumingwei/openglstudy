87d8d7c950eb8d769453ee848db3d6e4fb512b64
zhumingwei <zdf312192599@163.com>
2019-06-22 11:15:18 +0800

    纹理2

--- project/firstproject/firstproject/firstproject/firstproject.cpp
+++ project/firstproject/firstproject/firstproject/firstproject.cpp
@@ -85,9 +85,9 @@ int main()
 	glEnableVertexAttribArray(2);
 
 
-	unsigned int texture;
-	glGenTextures(1,&texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	unsigned int texture1,texture2;
+	glGenTextures(1,&texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
 	//纹理环绕方式 s,t,r
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -96,7 +96,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width,height,nrChannels;
-
+	stbi_set_flip_vertically_on_load(true);
 	unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
 	if (data)
 	{
@@ -105,10 +105,32 @@ int main()
 	}
 	else
 	{
+		std::cout << "Failed to load texture1" << std::endl;
+	}
+	stbi_image_free(data);
+
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	//纹理环绕方式 s,t,r
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//纹理过滤
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
 		std::cout << "Failed to load texture" << std::endl;
 	}
-	//stbi_image_free(data);
-	
+	stbi_image_free(data);
+	ourShader.use();
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	ourShader.setInt("texture1",0);
+	ourShader.setInt("texture2", 1);
 	//渲染循环
 	while (!glfwWindowShouldClose(window))
 	{
@@ -119,7 +141,10 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//bindtexture
-		glBindTexture(GL_TEXTURE_2D,texture);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D,texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 
 		ourShader.use();
 		glBindVertexArray(VAO);
